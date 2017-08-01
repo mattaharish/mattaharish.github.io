535eaac1de6928ad95db9f86e74214af56b8aa39
Harish Matta <harish.matta31@gmail.com>
2017-08-02 00:48:01 +0530

    Update houseCard.js

--- IceandFire/Angular/directives/houseCard.js
+++ IceandFire/Angular/directives/houseCard.js
@@ -1,7 +1,7 @@
 myApp.directive( "houseCard", function () {
   return {
     restrict: "E",
-    templateUrl: "../views/house-card.html",
+    templateUrl: "./views/house-card.html",
     controller: function ( $scope ) {
       //console.log( "Directive Scope: Character" );
       //console.log( $scope.item.name );
