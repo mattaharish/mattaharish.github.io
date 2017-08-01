3c9c71a53307a9876e26e641516c26c0a1e04afd
Harish Matta <harish.matta31@gmail.com>
2017-08-02 00:45:49 +0530

    Update bookSingleView.js

--- IceandFire/Angular/directives/bookSingleView.js
+++ IceandFire/Angular/directives/bookSingleView.js
@@ -1,7 +1,7 @@
 myApp.directive( "bookSingleView", function () {
   return {
     restrict: "E",
-    templateUrl: "../views/book-single-view.html",
+    templateUrl: "./views/book-single-view.html",
     controller: function ( $scope ) {
       //console.log( "Directive Scope: Character" );
       //console.log( $scope.singleData.name );
