c93cc7d07546481ad3721868e4b019acd5306514
Harish Matta <harish.matta31@gmail.com>
2017-08-02 00:47:30 +0530

    Update characterSingleView.js

--- IceandFire/Angular/directives/characterSingleView.js
+++ IceandFire/Angular/directives/characterSingleView.js
@@ -1,7 +1,7 @@
 myApp.directive( "characterSingleView", function () {
   return {
     restrict: "E",
-    templateUrl: "../views/character-single-view.html",
+    templateUrl: "./views/character-single-view.html",
     controller: function ( $scope ) {
       //console.log( "Directive Scope: Character" );
       //console.log( $scope.singleData.name );
