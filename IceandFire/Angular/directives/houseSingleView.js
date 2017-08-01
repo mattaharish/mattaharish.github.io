5cd089e8f67d7b16603d23700523211f7d61207e
Harish Matta <harish.matta31@gmail.com>
2017-08-02 00:48:42 +0530

    Update houseSingleView.js

--- IceandFire/Angular/directives/houseSingleView.js
+++ IceandFire/Angular/directives/houseSingleView.js
@@ -1,7 +1,7 @@
 myApp.directive( "houseSingleView", function () {
   return {
     restrict: "E",
-    templateUrl: "../views/house-single-view.html",
+    templateUrl: "./views/house-single-view.html",
     controller: function ( $scope ) {
       //console.log( "Directive Scope: Character" );
       //console.log( $scope.singleData.name );
