8cfa9bd671f76cf1a4b69ffe6944fc2ac10b96ec
mattaharish <harish.matta31@gmail.com>
2018-08-09 00:19:29 +0530

    Added menu button, actions and trasitions on menu button

---
+++ dist/js/main.js
@@ -0,0 +1,37 @@
+// Select Menu Button
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach((item) => {
+            item.classList.add('close');
+        });
+        
+        // Show Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach((item) => {
+            item.classList.remove('close');
+        });
+        
+        // Show Menu State
+        showMenu = false;
+    }
+}
